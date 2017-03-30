7220ecfd96d42c74e4165647bad3c98461d21347
luis <luisg5@yahoo.com.br>
2017-03-30 11:56:37 -0300

    Adicionado movimento ao quadrado

--- Sprite.js
+++ Sprite.js
@@ -5,6 +5,20 @@ function Sprite(){
 	this.vy = 0;
 	this.ax = 0;
 	this.ay = 0;
+	this.vm = 150;
 	this.g = 30;
+	this.vidas = 0;
 	this.color = "black";
+
+	this.mover = function(dt){
+		this.vx = this.vx + this.ax * dt;
+		this.vy = this.vy + (this.ay + this.g) * dt;
+		this.x = this.x + this.vx * dt;
+		this.y = this.y + this.vy * dt;
+	}
+
+	this.desenhar = function(ctx){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, 10, 10);
+	}
 }
