579300d27e55df02158763ac619cbb11df95596e
luis <luisg5@yahoo.com.br>
2017-03-31 16:15:01 -0300

    Ajustes

--- Sprite.js
+++ Sprite.js
@@ -9,6 +9,7 @@ function Sprite(){
 	this.g = 30;
 	this.vidas = 0;
 	this.nivel = 1;
+	this.largura = 0;
 	this.color = "black";
 
 	this.mover = function(dt){
@@ -19,53 +20,55 @@ function Sprite(){
 	}
 
 	this.desenhar = function(ctx){
-		if(pc.vidas > 0 && pc.nivel < 11){
+		if(this.vidas > 0 && this.nivel < 11){
 			ctx.fillStyle = this.color;
-			ctx.fillRect(this.x, this.y, 10, 10);
+			ctx.fillRect(this.x, this.y, this.largura, 10);
 		}
-		else if(pc.vidas == 0){
-				ctx.fillStyle = "orange";
+		else if(this.vidas == 0){
+				ctx.fillStyle = "#843232";
 				ctx.strokeStyle = "black";
-				ctx.font = "2em Arial";
+				ctx.font = "3em fantasy";
 				var texto = "Você Perdeu!";
-				ctx.fillText(texto, 100, 100);
-				ctx.strokeText(texto, 100, 100);
+				ctx.fillText(texto, 70, 100);
+				ctx.strokeText(texto, 70, 100);
 				ctx.fillStyle = "white";
 				ctx.strokeStyle = "black";
-				ctx.font = "1.1em sans-serif";
+				ctx.font = "1.4em fantasy";
 				var texto = "Tecle ENTER para reiniciar";
-				ctx.fillText(texto, 90, 130);
-				ctx.strokeText(texto, 90, 130);
+				ctx.fillText(texto, 80, 130);
+				ctx.strokeText(texto, 80, 130);
 				this.vx = 0;
 				this.vy = 0;
-			 }else if(pc.nivel > 10){
-					ctx.fillStyle = "orange";
+			 }else if(this.nivel > 10){
+					ctx.fillStyle = "gold";
 					ctx.strokeStyle = "black";
-					ctx.font = "2em Arial";
+					ctx.font = "3em fantasy";
 					var texto = "Você Venceu!";
-					ctx.fillText(texto, 100, 100);
-					ctx.strokeText(texto, 100, 100);
+					ctx.fillText(texto, 70, 100);
+					ctx.strokeText(texto, 70, 100);
 					ctx.fillStyle = "white";
 					ctx.strokeStyle = "black";
-					ctx.font = "1.1em sans-serif";
+					ctx.font = "1.4em fantasy";
 					var texto = "Tecle ENTER para reiniciar";
-					ctx.fillText(texto, 90, 130);
-					ctx.strokeText(texto, 90, 130);
+					ctx.fillText(texto, 80, 130);
+					ctx.strokeText(texto, 80, 130);
 					this.vx = 0;
 					this.vy = 0;
 				   }
 	}
 	
 	this.desenhaFPS = function(){
-		if(pc.nivel < 11 && pc.vidas > 0){
+		if(this.nivel < 11 && this.vidas > 0){
+			ctx.fillStyle = "white";
 			ctx.strokeStyle = "black";
 			ctx.font = "1em Arial Black";
+			ctx.fillText("Fps: " + Math.floor(1 / dt), 160, 20);
 			ctx.strokeText("Fps: " + Math.floor(1 / dt), 160, 20);
 		}
 	}
 	
 	this.desenhaVidas = function(){
-		if(pc.nivel < 11 && pc.vidas > 0){
+		if(this.nivel < 11 && this.vidas > 0){
 			ctx.fillStyle = "yellow";
 			ctx.strokeStyle = "red";
 			ctx.font = "1em Arial Black";
@@ -75,7 +78,7 @@ function Sprite(){
 	}
 
 	this.desenhaNivel = function(){
-		if(pc.nivel < 11 && pc.vidas > 0){
+		if(this.nivel < 11 && this.vidas > 0){
 			ctx.fillStyle = "yellow";
 			ctx.strokeStyle = "black";
 			ctx.font = "1em Arial Black";
