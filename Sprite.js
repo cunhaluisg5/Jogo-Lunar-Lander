d6a4916bca06b8aa01076c59aeb26d26bcb186f0
luis <luisg5@yahoo.com.br>
2017-03-31 11:10:26 -0300

    Algumas funções modificadas

--- Sprite.js
+++ Sprite.js
@@ -19,35 +19,70 @@ function Sprite(){
 	}
 
 	this.desenhar = function(ctx){
-		if(pc.vidas > 0){
+		if(pc.vidas > 0 && pc.nivel < 11){
 			ctx.fillStyle = this.color;
 			ctx.fillRect(this.x, this.y, 10, 10);
-		} else{
-			ctx.fillStyle = "orange";
+		}
+		else if(pc.vidas == 0){
+				ctx.fillStyle = "orange";
+				ctx.strokeStyle = "black";
+				ctx.font = "2em Arial";
+				var texto = "Você Perdeu!";
+				ctx.fillText(texto, 100, 100);
+				ctx.strokeText(texto, 100, 100);
+				ctx.fillStyle = "white";
+				ctx.strokeStyle = "black";
+				ctx.font = "1.1em sans-serif";
+				var texto = "Tecle ENTER para reiniciar";
+				ctx.fillText(texto, 90, 130);
+				ctx.strokeText(texto, 90, 130);
+				this.vx = 0;
+				this.vy = 0;
+			 }else if(pc.nivel > 10){
+					ctx.fillStyle = "orange";
+					ctx.strokeStyle = "black";
+					ctx.font = "2em Arial";
+					var texto = "Você Venceu!";
+					ctx.fillText(texto, 100, 100);
+					ctx.strokeText(texto, 100, 100);
+					ctx.fillStyle = "white";
+					ctx.strokeStyle = "black";
+					ctx.font = "1.1em sans-serif";
+					var texto = "Tecle ENTER para reiniciar";
+					ctx.fillText(texto, 90, 130);
+					ctx.strokeText(texto, 90, 130);
+					this.vx = 0;
+					this.vy = 0;
+				   }
+	}
+	
+	this.desenhaFPS = function(){
+		if(pc.nivel < 11 && pc.vidas > 0){
 			ctx.strokeStyle = "black";
-      		ctx.font = "2em Arial";
-      		var texto = "Você Perdeu!";
-      		ctx.fillText(texto, 100, 100);
-      		ctx.strokeText(texto, 100, 100);
-      		ctx.fillStyle = "white";
-      		ctx.strokeStyle = "black";
-      		ctx.font = "1.1em sans-serif";
-      		var texto = "Tecle ENTER para reiniciar";
-      		ctx.fillText(texto, 90, 130);
-      		ctx.strokeText(texto, 90, 130);
-      		this.vx = 0;
-      		this.vy = 0;
-      		pc.nivel = 1;
-		  }
+			ctx.font = "1em Arial Black";
+			ctx.strokeText("Fps: " + Math.floor(1 / dt), 160, 20);
+		}
+	}
+	
+	this.desenhaVidas = function(){
+		if(pc.nivel < 11 && pc.vidas > 0){
+			ctx.fillStyle = "yellow";
+			ctx.strokeStyle = "red";
+			ctx.font = "1em Arial Black";
+			ctx.fillText("Vidas: " + pc.vidas, 10, 20);
+			ctx.strokeText("Vidas: " + pc.vidas, 10, 20);
+		}
 	}
 
 	this.desenhaNivel = function(){
-		ctx.fillStyle = "yellow";
-	    ctx.strokeStyle = "black";
-	    ctx.font = "1em Arial Black";
-	    var texto = "Nível: " + this.nivel;
-	    ctx.fillText(texto, 320, 20);
-	    ctx.strokeText(texto, 320, 20);
+		if(pc.nivel < 11 && pc.vidas > 0){
+			ctx.fillStyle = "yellow";
+			ctx.strokeStyle = "black";
+			ctx.font = "1em Arial Black";
+			var texto = "Nível: " + this.nivel;
+			ctx.fillText(texto, 320, 20);
+			ctx.strokeText(texto, 320, 20);
+		}
 	}
 
 	this.colidiuComBase = function(alvo){
